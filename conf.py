e817df2fdcda062265e27325788d27a74ef7b1ae
Jonas Mouyal <jonasmouyal@gmail.com>
2022-04-25 22:18:12 +0300

    Changed chromedriver path

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ database = 'jonas_maria'
 
 
 # path to the driver used in selenium
-PATH = "/home/ubuntu/usr/local/bin/chromedriver"
+PATH = "/home/ubuntu/data_mining_project/chromedriver"
 service = Service(PATH)
 
 # header if we decide to write the data in a csv file
