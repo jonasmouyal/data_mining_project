e162ffacbd3f2b2290300f9ed27018dd3e80e2ca
Jonas Mouyal <jonasmouyal@gmail.com>
2022-04-25 20:27:15 +0300

    Added remote server connection to database.py

--- database.py
+++ database.py
@@ -1,21 +1,9 @@
 import conf as CFG
 
-connection = CFG.pymysql.connect(host='localhost',
-                                 user='root',
+connection = CFG.pymysql.connect(host=CFG.host,
+                                 user=CFG.root,
                                  password=CFG.password,
-                                 charset='utf8mb4',
-                                 cursorclass=CFG.pymysql.cursors.DictCursor)
-
-with connection:
-    with connection.cursor() as cursor:
-        # Create a new database
-        sql = 'CREATE DATABASE unity;'
-        cursor.execute(sql)
-
-connection = CFG.pymysql.connect(host='localhost',
-                                 user='root',
-                                 password=CFG.password,
-                                 database='unity',
+                                 database=CFG.database,
                                  charset='utf8mb4',
                                  cursorclass=CFG.pymysql.cursors.DictCursor)
 with connection:
@@ -26,7 +14,7 @@ with connection:
               'category_name VARCHAR(150), PRIMARY KEY (category_id));'
         cursor.execute(sql)
 
-        sql = 'CREATE TABLE editors (editor_id INT NOT NULL AUTO_INCREMENT, editor_name VARCHAR(150) NOT NULL UNIQUE, '\
+        sql = 'CREATE TABLE editors (editor_id INT NOT NULL AUTO_INCREMENT, editor_name VARCHAR(150) NOT NULL UNIQUE, ' \
               'website VARCHAR(150), ' \
               'email VARCHAR(150), number_of_assets INT,' \
               'PRIMARY KEY(editor_id)); '
@@ -45,7 +33,3 @@ with connection:
               'reviewer_name VARCHAR(150), ' \
               'rating_over_five INT, text LONGTEXT, PRIMARY KEY (review_id));'
         cursor.execute(sql)
-
-
-
-
